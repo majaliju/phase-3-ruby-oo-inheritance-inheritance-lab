8f23c2d2ed0f1e5c2698195c0e93b1a2bb9dc574
majaliju <majalijunyc@gmail.com>
2022-07-31 21:40:44 -0400

    commit tree de4074e4b3c9e47c5fe7e9ccc526516453f89a0d

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+
+class Student < User
+
+  attr_reader :knowledge
+
+  # def knowledge=(value)
+  #   @knowledge = []
+  # end
+
+  def initialize
+    @knowledge = []
+  end
+  
+
+  def learn (string)
+    @knowledge << string
+  end
+
+  # def knowledge 
+  #   @knowledge
+  # end
 
 end
